f903f0606cbd0ff9bc4ed8fa342d8ee0e3bb0b7b
Boris Kravchenko <115030529+allmycalifornia@users.noreply.github.com>
2023-09-11 13:59:10 +0300

    initial commit

--- Blank/AppDelegate.swift
+++ Blank/AppDelegate.swift
@@ -1,36 +1,43 @@
 //
 //  AppDelegate.swift
-//  Blank
+//  Navigation
 //
-//  Created by Борис Кравченко on 11.09.2023.
+//  Created by Борис Кравченко on 31.01.2023.
 //
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootViewController = MyViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Вызывается, когда приложение переходит в неактивное состояние (например, входящий звонок или SMS)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Вызывается, когда приложение уходит в фоновый режим
     }
 
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Вызывается, когда приложение возвращается из фонового режима в активное состояние
+    }
 
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Вызывается, когда приложение становится активным после неактивного состояния
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Вызывается, когда приложение завершает свою работу
+    }
 }
-
